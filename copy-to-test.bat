6b8a688b3fa85a47cfc3e4423a7b7ecf7c910d11
Ruiqi Tian <rtian25@uwo.ca>
2020-04-03 14:08:20 -0400

    updated copy script to include subfolders

---
+++ copy-to-test.bat
@@ -0,0 +1,19 @@
+@echo off
+xcopy .\client ..\se3314-assn3-testing\p1\client /y /s
+xcopy .\server ..\se3314-assn3-testing\p1\server /y /s
+
+xcopy .\client ..\se3314-assn3-testing\p2\client /y /s
+xcopy .\server ..\se3314-assn3-testing\p2\server /y /s
+
+xcopy .\client ..\se3314-assn3-testing\p3\client /y /s
+xcopy .\server ..\se3314-assn3-testing\p3\server /y /s
+
+xcopy .\client ..\se3314-assn3-testing\p4\client /y /s
+xcopy .\server ..\se3314-assn3-testing\p4\server /y /s
+
+xcopy .\client ..\se3314-assn3-testing\p5\client /y /s
+xcopy .\server ..\se3314-assn3-testing\p5\server /y /s
+
+xcopy .\client ..\se3314-assn3-testing\p6\client /y /s
+xcopy .\server ..\se3314-assn3-testing\p6\server /y /s
+exit
